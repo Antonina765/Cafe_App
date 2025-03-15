51bcf42df8312cd11378050d616aa576ce026b1c
Antonina765 <antoninakapuza0@gmail.com>
2025-03-15 17:26:48 +0300

    change location for styles

--- Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
+++ Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da6a7305f3cd8f83db0b8185569c25091e17d5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbd7dec0e8740eba17b33368062fb313de36612")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
