34bd75df7e454d24a6a493e49440efb627381876
Antonina765 <antoninakapuza0@gmail.com>
2025-03-16 21:26:09 +0300

    change something in desegn

--- Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
+++ Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc003817277e72d92d0cce2bd800e945bf29e1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1bec592b71429f084ca021d4aac19ae5756f34a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
