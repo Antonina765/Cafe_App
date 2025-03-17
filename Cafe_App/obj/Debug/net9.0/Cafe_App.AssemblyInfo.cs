ee6402c931e5abacbcabd20b0dad3ca14aa52f9d
Antonina765 <antoninakapuza0@gmail.com>
2025-03-17 17:42:54 +0300

    add some cafe

--- Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
+++ Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3284c130f729218371399f8c51454e6e72cb9e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f1bdd1686f61da2dbda7f0524f0d83ff055ea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
