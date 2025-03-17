a3284c130f729218371399f8c51454e6e72cb9e8
Antonina765 <antoninakapuza0@gmail.com>
2025-03-17 17:22:43 +0300

    end design

--- Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
+++ Cafe_App/obj/Debug/net9.0/Cafe_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf03c8d59ff8ee859ffebb326fbf705ab5530521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34d565db0d3351c4aee8eceb62d75113623b8a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafe_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
